3740e289e1064ad1bf53663a146c25c0c9cb92f6
Maryoto <kevinnjooo@gmail.com>
2026-08-03 16:07:07 +0700

    Initial commit

---
+++ HoMoold/HoMooldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoMooldApp.swift
+//  HoMoold
+//
+//  Created by Kevin Joseph Handoyo on 03/08/26.
+//
+
+import SwiftUI
+
+@main
+struct HoMooldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
